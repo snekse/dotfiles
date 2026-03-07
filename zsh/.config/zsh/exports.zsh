bc8b441f2fdccb2f88c661941a9329f46967dd9b
Derek Eskens <snekse@gmail.com>
2026-03-07 17:45:58 -0600

    feat: Configure Homebrew to auto-update weekly.

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -8,6 +8,9 @@ export HISTFILESIZE=1000000
 export HISTFILE="$HOME/.zsh_history"
 export HISTIGNORE="clear:bg:fg:cd:cd -:exit:date:w:* --help"
 
+# Homebrew — auto-update at most once a week (default is 300s / 5 minutes)
+export HOMEBREW_AUTO_UPDATE_SECS=604800
+
 # Color
 export CLICOLOR=1
 export GREP_COLOR='1;32'
